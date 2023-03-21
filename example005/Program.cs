d3058157f62477a6a519be3943a8662cccf21ef4
kirill <golovinkiruxa@gmail.com>
2023-03-21 18:37:07 +0100

    example005_If

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("write username:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+  Console.WriteLine("Wow, it is MASHA!!!");
+}
+else
+{
+  Console.Write("Hello, ");
+  Console.WriteLine(username);
+}
