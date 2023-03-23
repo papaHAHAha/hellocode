8df36b8647428d37e8dc09be008ec11d2f3db535
kirill <golovinkiruxa@gmail.com>
2023-03-23 12:42:52 +0100

    zadanie4

---
+++ seminar 1 c#/4zadanie/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+//  и на выходе показывает **последнюю** цифру этого числа.
+
+//     456 -> 6
+
+//     782 -> 2
+
+//     918 -> 8
+Console.Clear();
+Console.WriteLine("write number");
+int a = int.Parse(Console.ReadLine());
+int b = a % 10;
+Console.Write(b);
+
