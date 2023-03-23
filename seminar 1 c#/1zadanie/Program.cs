256670fccc1aa531c6b83a2ce45afe64b49bf3d9
kirill <golovinkiruxa@gmail.com>
2023-03-23 12:00:53 +0100

    1_and_2zadanie_seminara

---
+++ seminar 1 c#/1zadanie/Program.cs
@@ -0,0 +1,20 @@
+﻿/***Задача 0:** 
+
+**Условие:**
+
+Напишите программу, которая на вход принимает число и 
+выдаёт его квадрат (число умноженное на само себя).
+
+**Например:**
+
+4 -> 16
+-3 -> 9
+-7 -> 49*/
+
+Console.Clear();
+Console.Write("Введите число");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+int sqr1 = Convert.ToInt32(Math.Pow(number, 3));
+Console.WriteLine($"Квадрат числа {number} равен {sqr1}");
---
+++ seminar 1 c#/2zadanie/Program.cs
@@ -0,0 +1,21 @@
+﻿/*1.Напишите программу, которая на вход принимает два числа и проверяет,
+ является ли первое число квадратом второго.
+
+a = 25; b = 5->да
+a = 2 b = 10->нет
+a = 9; b = -3->да
+a = -3 b = 9->нет*/
+Console.Write("write numA");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("write numB");
+int numB = int.Parse(Console.ReadLine());
+int sqr = numA * numA;
+
+if (numB == sqr)
+{
+  Console.WriteLine("a квадрат b");
+}
+else
+{
+  Console.WriteLine("a не квадрат b");
+}
