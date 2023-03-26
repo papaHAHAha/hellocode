633fdddf41af080ac3b032f56a2e55b654699658
kirill <golovinkiruxa@gmail.com>
2023-03-26 05:04:59 +0200

    DOMASHNEE_TASK2

---
+++ seminar 1 c#/DOMASHNEE/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("write first number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("write second number");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+
+if (a > b)
+{
+  max = a;
+  Console.WriteLine($"max number = {max}");
+}
+if (a < b)
+{
+  max = b;
+  Console.WriteLine($"max number = {max}");
+}
