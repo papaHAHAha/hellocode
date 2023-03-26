1e2905db39b0d288bdd2e6d71cc80efe4f301451
kirill <golovinkiruxa@gmail.com>
2023-03-26 20:02:51 +0200

    DOMASHNEE_TASK8

---
+++ seminar 1 c#/DOMASHNEE/task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("write N");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i < n + 1 ; i++)
+{
+  if((i % 2) == 0)
+ {
+  Console.WriteLine(i);
+ }
+}
+
+
+
+
