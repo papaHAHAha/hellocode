3ddf2376fe73084b22c45da03c4607f6d10fd109
kirill <golovinkiruxa@gmail.com>
2023-03-26 19:32:21 +0200

    DOMASHNEE_TASK6

---
+++ seminar 1 c#/DOMASHNEE/task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("write number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a % 2) == 0)
+Console.WriteLine("четное");
+else
+  Console.WriteLine("нечетное");
+
