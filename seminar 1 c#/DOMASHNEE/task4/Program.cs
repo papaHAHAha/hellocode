6461788350ae4d5b0c9f40eaf34ab51474cdffbe
kirill <golovinkiruxa@gmail.com>
2023-03-26 19:04:24 +0200

    DOMASHNEE_TASK4

---
+++ seminar 1 c#/DOMASHNEE/task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("write first number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("write second number");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("write third number");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"max = {max}");
