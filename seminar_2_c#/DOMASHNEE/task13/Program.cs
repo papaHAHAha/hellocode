2d2557e3f002751f15c9393b04b7758cedcd6197
kirill <golovinkiruxa@gmail.com>
2023-03-29 05:51:31 +0200

    task13_DOMASHNEE

---
+++ seminar_2_c#/DOMASHNEE/task13/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("write number");
+int a = int.Parse(Console.ReadLine());
+while (a > 999)
+{
+  a/= 10;
+}
+int b = a%10;
+Console.WriteLine(b);
+//  int b(int a){
+//   while (a > 999)
+//   {
+//   a /= 10;
+//   }
+//   return a % 10;
+//  }
+// Console.WriteLine(b(a));
