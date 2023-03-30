e09f1fbbbc7b456d2e2d012be2184c8b5e155d94
kirill <golovinkiruxa@gmail.com>
2023-03-30 13:46:25 +0200

    task15_DOMASHNEE

--- seminar_2_c#/DOMASHNEE/task13/Program.cs
+++ seminar_2_c#/DOMASHNEE/task13/Program.cs
@@ -2,17 +2,17 @@
 Console.Clear();
 Console.WriteLine("write number");
 int a = int.Parse(Console.ReadLine());
-while (a > 999)
-{
-  a/= 10;
-}
-int b = a%10;
-Console.WriteLine(b);
-//  int b(int a){
-//   while (a > 999)
-//   {
-//   a /= 10;
-//   }
-//   return a % 10;
-//  }
-// Console.WriteLine(b(a));
+// while (a > 999)
+// {
+//   a/= 10;
+// }
+// int b = a%10;
+// Console.WriteLine(b);
+ int b(int a){
+  while (a > 999)
+  {
+  a /= 10;
+  }
+  return a % 10;
+ }
+Console.WriteLine(b(a));
---
+++ seminar_2_c#/DOMASHNEE/task15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Clear();
+Console.Write("write number of day: ");
+int a = int.Parse(Console.ReadLine());
+if (a == 6 || a == 7)
+{
+  Console.WriteLine($"день {a} -> выходной");
+}
+if (a > 0 && a <= 5)
+{
+  Console.WriteLine($"день {a} -> не выходной");
+}
+if (a < 0 || a > 7)
+{
+  Console.WriteLine("в неделе 7 дней");
+  return;
+}
