8d2e48411a2a2f5b8316f8f829e6f9e79b6aae45
kirill <golovinkiruxa@gmail.com>
2023-03-30 13:18:10 +0200

    zadanie20(2)_seminar3c#

---
+++ seminar_3_c#/zadanie20(2)/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+
+// 2 -> 1,4
+Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+for (int i = 1; i < a + 1; i++)
+{
+  Console.Write($"{Math.Pow(i, 2)} ");
+}
