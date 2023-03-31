2d60f13b08472af23503c4e0ca1a6d39b360e244
kirill <golovinkiruxa@gmail.com>
2023-04-01 01:58:32 +0200

    task21_DOMASHNEE

---
+++ seminar_3_c#/DOMASHNEE/task21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.Clear();
+Console.WriteLine("write x");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("write y");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine("write z");
+double z = double.Parse(Console.ReadLine());
+Console.WriteLine("write x1");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("write y1");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("write z1");
+double z1 = double.Parse(Console.ReadLine());
+double sqrt = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) + (z - z1) * (z - z1));
+Console.WriteLine($"{sqrt:f2}");
