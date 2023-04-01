22c82e8e081bd607ced5da0927b00f7915840df1
kirill <golovinkiruxa@gmail.com>
2023-04-01 02:04:45 +0200

    task23_DOMASHNEE

--- seminar_3_c#/DOMASHNEE/task23/Program.cs
+++ seminar_3_c#/DOMASHNEE/task23/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.Write("write N: ");
+int n = int.Parse(Console.ReadLine());
+for(int i=1; i <n+1; i++)
+{
+  Console.Write($"{Math.Pow(i, 3)} ");
+}
