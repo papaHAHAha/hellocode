f1a4bf419806dd8c703ffb13df4458e36538b758
kirill <golovinkiruxa@gmail.com>
2023-04-03 12:08:15 +0200

    zadanie24_seminar4

---
+++ seminar_4_C#/zadanie24/Program.cs
@@ -0,0 +1,12 @@
+﻿int GetSum(int n){
+  int sum = 0;
+  for (int i = 1; i<=n;i++)
+    sum += i; //<=> sum = sum + i
+  return sum;
+}
+
+Console.Clear();
+Console.Write("write number: ");
+int n = int.Parse(Console.ReadLine());
+int a = GetSum(n);
+Console.WriteLine($"Сумма от 1 до {n} равна {GetSum(n)} ");
