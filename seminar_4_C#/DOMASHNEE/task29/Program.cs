c33a25975618396b77a246d75d6e932d030c1738
kirill <golovinkiruxa@gmail.com>
2023-04-05 22:02:05 +0200

    task29_DOMASHNEE

--- seminar_4_C#/DOMASHNEE/task29/Program.cs
+++ seminar_4_C#/DOMASHNEE/task29/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] NewArray(int size)
+{
+  int[] count = new int[size];
+for (int i = 0; i < size; i++){
+  count[i] = new Random().Next(1, 999);
+}
+return count;
+}
+int[] array = NewArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
