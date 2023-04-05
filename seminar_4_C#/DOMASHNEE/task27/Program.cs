a81cb97b05f5a8913b1a5060fdf9e22b60fa88a8
kirill <golovinkiruxa@gmail.com>
2023-04-05 22:11:55 +0200

    task27_domashnee

--- seminar_4_C#/DOMASHNEE/task27/Program.cs
+++ seminar_4_C#/DOMASHNEE/task27/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("write number");
+int a = int.Parse(Console.ReadLine());
+int n = a;
+int sum = 0;
+while(n>0){
+  sum += n%10;
+  n = n/10;
+}
+Console.WriteLine($"{a} -> {sum}");
