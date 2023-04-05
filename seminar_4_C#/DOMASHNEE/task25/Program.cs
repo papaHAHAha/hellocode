0977a76bb143ebf7f843b3b11c3b91f132f7b951
kirill <golovinkiruxa@gmail.com>
2023-04-05 19:43:39 +0200

    domashnee_task25

---
+++ seminar_4_C#/DOMASHNEE/task25/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("write first number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("write second number");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a}, {b} -> {Math.Pow(a, b)}");
