8f2ae5bc1746f532ab1b0d712ec89c1eea7afbbd
kirill <golovinkiruxa@gmail.com>
2023-04-07 15:28:16 +0200

    task34_DOMASHNEE

---
+++ seminar_5_c#/DOMASHNEE/task34/Program.cs
@@ -0,0 +1,25 @@
+﻿// // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве. 
+// [345, 897, 568, 234] -> 2
+int[] newArray(int size)
+{
+  int[] res = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    res[i] = new Random().Next(100, 999);
+  }
+  return res;
+}
+int GetEvenNumber (int[] array)
+{
+  int sum = 0;
+  foreach(int item in array)
+  {
+    if(item % 2 == 0) sum++;
+  }
+  return sum;
+}
+Console.Clear();
+int[] startArray = newArray(4);
+Console.WriteLine(String.Join(" ", startArray));
+Console.WriteLine($"колво четных чисел в массиве = {GetEvenNumber(startArray)}");
