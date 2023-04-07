8d269ab2ecdc9488e32811f48e489153745f299f
kirill <golovinkiruxa@gmail.com>
2023-04-07 16:58:59 +0200

    task36_DOMASHNEE

--- seminar_5_c#/DOMASHNEE/task36/Program.cs
+++ seminar_5_c#/DOMASHNEE/task36/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+int[] getArray(int size, int minValue, int maxValue)
+{
+  int[] res = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    res[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return res;
+}
+int sumUnevenIndex(int[] array)
+{
+  int sum = 0;
+  for (int i = 1; i < array.Length; i += 2)
+  {
+    sum = sum + array[i];
+  }
+  return sum;
+}
+Console.Clear();
+int[] startArray = getArray(4, 1, 20);
+Console.WriteLine(String.Join(" ", startArray));
+Console.WriteLine($"Сумма элементов нечетных позиций = {sumUnevenIndex(startArray)}");
+
+
--- seminar_5_c#/DOMASHNEE/task38/Program.cs
+++ seminar_5_c#/DOMASHNEE/task38/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+int[] getArray(int size, int minValue, int maxValue)
+{
+  int[] res = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    res[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return res;
+}
+int FindMax(int[] array)
+{
+  int max = array[1];
+  foreach (int item in array)
+  {
+    if (item > max) max = item;
+  }
+  return max;
+}
+int FindMin(int[] array)
+{
+  int min = array[1];
+  foreach(int item in array)
+  {
+  if (item < min) min = item;
+  }
+  return min;
+}
+Console.Clear();
+int[] startArray = getArray(4, 1, 20);
+Console.WriteLine($"[{String.Join(", ", startArray)}]";
+Console.WriteLine($"MAX = {FindMax(startArray)}");
+Console.WriteLine($"MIN = {FindMin(startArray)}");
+Console.WriteLine($"разница между максимальным и минимальным = {FindMax(startArray) - FindMin(startArray)}");
