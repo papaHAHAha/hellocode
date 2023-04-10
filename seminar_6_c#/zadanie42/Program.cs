a82207618c2814270a9a13d4609e85da036e7c79
kirill <golovinkiruxa@gmail.com>
2023-04-10 13:01:45 +0200

    zadanie42_seminar6

---
+++ seminar_6_c#/zadanie42/Program.cs
@@ -0,0 +1,21 @@
+﻿// **Задача 42:**Напишите программу,
+//  которая будет преобразовывать десятичное число в двоичное.
+
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+Console.WriteLine("write number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a} -> {BinaryNumber(a)}");
+string BinaryNumber(int a)
+{
+  string res = "";
+  string nums = "01";
+  while (a > 0)
+  {
+    res = nums[a % 2] + res;
+    a /=2;
+  }
+  return res;
+}
+
