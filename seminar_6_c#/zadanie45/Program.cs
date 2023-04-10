28a2964f595dc291d7103c8734c06199e9fee3f7
kirill <golovinkiruxa@gmail.com>
2023-04-10 13:45:17 +0200

    zadanie45

---
+++ seminar_6_c#/zadanie45/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 45: Напишите программу,
+которая будет создавать копию заданного массива
+с помощью поэлементного копирования.*/
+Console.Clear();
+int[] GetArray(int minValue, int maxValue, int size)
+{
+  int[] arr = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    arr[i] = new Random().Next(minValue, maxValue + 1);
+  }
+  return arr;
+}
+int[] CopyArr(int[] arr)
+{
+  int[] copy = new int[arr.Length];
+  for(int i = 0; i < arr.Length;i++){
+    copy[i] = arr[i];
+  }
+  return copy;
+}
+int[] Array = GetArray(0,10,10);
+Console.WriteLine($"new array -> [{String.Join(", ", Array)}]");
+int[] CopyArray = CopyArr(Array);
+Console.WriteLine($"copy -> [{String.Join(", ", Array)}]");
