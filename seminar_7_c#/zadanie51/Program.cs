75940fe5fc6efe2bcc3625b2012ea2e217300ce4
kirill <golovinkiruxa@gmail.com>
2023-04-13 13:18:27 +0200

    zadanie51

--- seminar_7_c#/zadanie51/Program.cs
+++ seminar_7_c#/zadanie51/Program.cs
@@ -6,3 +6,57 @@
 // 5 9 2 3
 // 8 4 2 4
 // Сумма элементов главной диагонали: 1+9+2 = 12
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($" Сумма элементов главной диагонали -> {GetSum(array)}");
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      result[i, j] = new Random().Next(min, max + 1);
+    }
+
+  }
+  return result;
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int GetSum(int[,] array)
+{
+  int sum = 0;
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (i == j)
+      {
+        sum += array[i, j];
+      }
+    }
+  }
+  return sum;
+}
