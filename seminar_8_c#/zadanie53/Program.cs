3c7c7fb8ace4d53d19a6144b444d0839e7cb67d0
kirill <golovinkiruxa@gmail.com>
2023-04-17 22:29:26 +0200

    zadanie53

---
+++ seminar_8_c#/zadanie53/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задайте двумерный массив. Напишите программу, 
+//которая поменяет местами первую и последнюю строку массива.
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 10, 100);
+PrintArray(array);
+WriteLine();
+Write("Введите строку 1: ");
+int a1 = int.Parse(ReadLine());
+Write("Введите строку 2: ");
+int b1 = int.Parse(ReadLine());
+ChangeRows(array, a1, b1);
+PrintArray(array);
+
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      result[i, j] = new Random().Next(min, max + 1);
+    }
+
+  }
+  return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Write($"{inArray[i, j]} ");
+    }
+    WriteLine();
+  }
+}
+
+void ChangeRows(int[,] inArray, int a, int b)
+{
+  for (int i = 0; i < inArray.GetLength(1); i++)
+  {
+    int k = inArray[a - 1, i];
+    inArray[a - 1, i] = inArray[b - 1, i];
+    inArray[b - 1, i] = k;
+  }
+}
---
+++ seminar_8_c#/zadanie59/Program.cs
@@ -0,0 +1,60 @@
+﻿/*Задача 59: Из двумерного массива целых чисел удалить строку и столбец,
+ на пересечении которых расположен наименьший элемент.*/
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+PrintArray(array);
+WriteLine();
+WriteLine("Min Element in array: {0}", MinElement(array));
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Write($"{inArray[i, j]} ");
+    }
+    WriteLine();
+  }
+}
+int MinElement(int[,] array)
+{
+  int MinElement = array[0, 0];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (MinElement > array[i,j])
+      {
+        MinElement = array[i,j];
+      }
+    }
+  }
+  return MinElement;
+}
