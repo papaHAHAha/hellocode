438865e0ebb31684379e6c552dc13a90c803616e
kirill <golovinkiruxa@gmail.com>
2023-04-20 03:12:48 +0200

    task60

--- seminar_8_c#/DOMASHNEE/task60/Program.cs
+++ seminar_8_c#/DOMASHNEE/task60/Program.cs
@@ -4,3 +4,69 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+using System;
+using static System.Console;
+Clear();
+Write("x: ");
+int x = int.Parse(ReadLine());
+Write("y: ");
+int y = int.Parse(ReadLine());
+Write("z: ");
+int z = int.Parse(ReadLine());
+
+int[,,] array = new int[x, y, z];
+
+GetArray(array);
+PrintArray(array);
+
+void GetArray(int[,,] array)
+{
+  int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+  int number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+        number = temp[i];
+      }
+    }
+  }
+  int count = 0;
+  for (int x = 0; x < array.GetLength(0); x++)
+  {
+    for (int y = 0; y < array.GetLength(1); y++)
+    {
+      for (int z = 0; z < array.GetLength(2); z++)
+      {
+        array[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+
+void PrintArray(int[,,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        Write($"{array[i, j, k]}({i},{j},{k}) ");
+      }
+      WriteLine();
+    }
+    WriteLine();
+  }
+}
