1f33106e0d14b2dcf4de41205d6cff5d2cd2d965
kirill <golovinkiruxa@gmail.com>
2023-04-20 13:00:54 +0200

    zadanie_63_and_65

---
+++ seminar_9_c#/zadanie63/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+using System;
+using static System.Console;
+Clear();
+Write("Write first number: ");
+int number1 = int.Parse(ReadLine());
+Write("Write second number: ");
+int number2 = int.Parse(ReadLine());
+Write(WriteNow(number1, number2));
+
+/*void WriteNow(int number1, int number2)
+{
+  if (number1 <= number2)
+  {
+    Write(number1 + ", ");
+    number1++;
+    WriteNow(number1, number2);
+  }
+}*/
+string WriteNow(int number1, int nubmer2)
+{
+  if (number1 == number2)
+  {
+    return number1.ToString();
+  }
+  return number1+", "+ WriteNow(++number1, number2);
+}
