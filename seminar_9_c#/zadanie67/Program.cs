f65e26ff1290c8a7aea083e0e0a516679b83f0bf
kirill <golovinkiruxa@gmail.com>
2023-04-20 13:34:22 +0200

    zadanie67_and_69

---
+++ seminar_9_c#/zadanie67/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+
+WriteLine($"{SumNumbers(number)}");
+int SumNumbers(int number)
+{
+  if (number == 0)
+    return 0;
+  return (number % 10 + SumNumbers(number / 10));
+}
+
---
+++ seminar_9_c#/zadanie69/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа A и B,
+и возводит число А в целую степень B с помощью рекурсии.*/
+using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+Write("Введите степень числа: ");
+int pow = int.Parse(ReadLine());
+
+WriteLine($"{PowNumbers(number, pow)}");
+int PowNumbers(int number, int pow)
+{
+  if (pow == 0)
+    return 1;
+  return PowNumbers(number, --pow) * number;
+}
