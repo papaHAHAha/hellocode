3a9d22a9202af6f6bdbb1d8a3b10109e9f233f83
kirill <golovinkiruxa@gmail.com>
2023-04-21 21:09:38 +0200

    task58_62_DOMASHNEE

--- seminar_8_c#/DOMASHNEE/task58/Program.cs
+++ seminar_8_c#/DOMASHNEE/task58/Program.cs
@@ -1,7 +1,75 @@
 ﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // Например, даны 2 матрицы:
-// 2 4 | 3 4
-// 3 2 | 3 3
+// 2 4 | 3 4 \ 2*3 + 4*3 = 18 | 2*4 + 4*3 = 20
+// 3 2 | 3 3 \ 3*3 + 2*3 = 15 | 3*4 + 2*3 = 18
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+using System;
+using static System.Console;
+Clear();
+Write("Write rows first matrix: ");
+int m1 = int.Parse(ReadLine());
+Write("Write columns first matrix: ");
+int n1 = int.Parse(ReadLine());
+Write("Write rows second matrix: ");
+int m2 = int.Parse(ReadLine());
+Write("Write columns second matrix: ");
+int n2 = int.Parse(ReadLine());
+
+int[,] MatrixN1 = GetArray(m1, n1, 1, 5);
+int[,] MatrixN2 = GetArray(m2, n2, 1, 5);
+WriteLine("first matrix: ");
+PrintArray(MatrixN1);
+WriteLine("second matrix: ");
+PrintArray(MatrixN2);
+WriteLine();
+if (n1 != m2)
+{
+  WriteLine("matrices cannot be multiplied");
+}
+else
+{
+  int[,] ResultMatrix = new int[n1, m2];
+  MultiplyMatrix(MatrixN1, MatrixN2, ResultMatrix);
+  PrintArray(ResultMatrix);
+}
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Write($"{inArray[i, j]} ");
+    }
+    WriteLine();
+  }
+}
+void MultiplyMatrix(int[,] MatrixN1, int[,] MatrixN2, int[,] ResultMatrix)
+{
+  for (int i = 0; i < ResultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < ResultMatrix.GetLength(1); j++)
+    {
+      int multiply = 0;
+      for(int n = 0; n<MatrixN1.GetLength(1); n++)
+      {
+        multiply += MatrixN1[i,n] * MatrixN2[n,j];
+      }
+      ResultMatrix[i,j] = multiply;
+    }
+  }
+}
--- seminar_8_c#/DOMASHNEE/task62/Program.cs
+++ seminar_8_c#/DOMASHNEE/task62/Program.cs
@@ -4,4 +4,63 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+using System;
+using static System.Console;
+int RowsColumns = 4;
+int[,] SpiralArray = GetSpiralArray(RowsColumns);
+WriteLine();
+PrintArray(SpiralArray);
 
+
+
+void PrintArray(int[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Write(inArray[i, j] + "\t");
+    }
+    WriteLine();
+  }
+}
+
+int[,] GetSpiralArray(int size)
+{
+  int[,] Spiral = new int[size, size];
+  int i = 0;
+  int j = 0;
+  int number = 1;
+  while (size != 0)
+  {
+    int count = 0;
+    do
+    {
+      Spiral[i, j++] = number++;
+    }
+    while (++count < size - 1);
+    for (count = 0; count < size - 1; count++)
+    {
+      Spiral[i++, j] = number++;
+    }
+    for (count = 0; count < size - 1; count++)
+    {
+      Spiral[i, j--] = number++;
+    }
+    for (count = 0; count < size - 1; count++)
+    {
+      Spiral[i--, j] = number++;
+    }
+    ++i;
+    ++j;
+    if (size < 2)
+    {
+      size = 0;
+    }
+    else
+    {
+      size -= 2;
+    }
+  }
+  return Spiral;
+}
