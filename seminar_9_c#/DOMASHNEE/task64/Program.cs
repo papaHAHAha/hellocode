d8a08c5af2b37a40797d9fd35cb83bc565c37a5f
kirill <golovinkiruxa@gmail.com>
2023-04-25 05:18:27 +0200

    task64_domashnee

---
+++ seminar_9_c#/DOMASHNEE/task64/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+using System;
+using static System.Console;
+Clear();
+Write("write N: ");
+int N = int.Parse(ReadLine());
+
+WriteNow(N);
+
+void WriteNow(int N)
+{
+  if (N >= 1)
+  {
+    Write(N + ", ");
+    N--;
+    WriteNow(N);
+  }
+}
