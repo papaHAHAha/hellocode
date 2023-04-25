577406f377917e960e3f0e6d9fbcb385da51ae52
kirill <golovinkiruxa@gmail.com>
2023-04-25 05:41:00 +0200

    task66_domashnee

--- seminar_9_c#/DOMASHNEE/task66/Program.cs
+++ seminar_9_c#/DOMASHNEE/task66/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+using System;
+using static System.Console;
+Clear();
+Write("write m: ");
+int m = int.Parse(ReadLine());
+Write("write n: ");
+int n = int.Parse(ReadLine());
+WriteLine($"{sum(m, n)}");
+
+int sum(int m, int n)
+{
+  if (m == n)
+    return m;
+  else
+    m++;
+  return sum(m,n) + m - 1;
+}
